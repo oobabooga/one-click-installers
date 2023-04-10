dc11c3e8bf7eb67a589399efac625a2e263be597
BlueprintCoding <130100872+BlueprintCoding@users.noreply.github.com>
2023-04-09 21:23:35 -0600

    accidentally uploaded wrong file. Restoring original

--- start-webui.bat
+++ start-webui.bat
@@ -11,45 +11,9 @@ if not exist "%MAMBA_ROOT_PREFIX%\condabin\micromamba.bat" (
   call "%MAMBA_ROOT_PREFIX%\micromamba.exe" shell hook >nul 2>&1
 )
 call "%MAMBA_ROOT_PREFIX%\condabin\micromamba.bat" activate "%INSTALL_ENV_DIR%" || ( echo MicroMamba hook not found. && goto end )
+cd text-generation-webui
 
-:choose_extensions
-cd text-generation-webui\extensions
-@echo Available extensions:
-setlocal enabledelayedexpansion
-set count=0
-for /D %%d in (*) do (
-  set /A count+=1
-  set "extension[!count!]=%%d"
-  echo !count!: %%d
-)
-
-set /p choices=Enter the numbers of the extensions to use (separate multiple numbers with a space):
-
-set extensions=
-for %%c in (%choices%) do (
-  set "ext=!extension[%%c]!"
-  if defined extensions (
-    set "extensions=!extensions! !ext!"
-  ) else (
-    set "extensions=!ext!"
-  )
-)
-
-cd ..
-
-set command=python server.py --auto-devices --model vicuna-13b-GPTQ-4bit-128g --wbits 4 --groupsize 128 --listen --listen-port 7861 --extensions %extensions%
-
-@echo The following command will be run:
-@echo %command%
-
-set /p confirm=Do you want to run this command? (y/n):
-
-if /i "%confirm%"=="y" (
-  %command%
-) else (
-  cd ..
-  goto choose_extensions
-)
+call python server.py --auto-devices --cai-chat
 
 :end
 pause
