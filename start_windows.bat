d1a0e77872f105f44ea68a6aaad5e520e8dc1489
jllllll <3887729+jllllll@users.noreply.github.com>
2023-04-15 18:21:44 -0500

    Add check for spaces in path
    Installation of Miniconda will fail in this case

--- start_windows.bat
+++ start_windows.bat
@@ -2,6 +2,8 @@
 
 cd /D "%~dp0"
 
+echo "%CD%"| findstr /C:" " >nul && echo This script relies on Miniconda which can not be silently installed under a path with spaces. && goto end
+
 set PATH=%PATH%;%SystemRoot%\system32
 
 @rem config
