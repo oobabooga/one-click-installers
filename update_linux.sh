981fc41fb5caaada36756a07430481902c2d6781
jllllll <3887729+jllllll@users.noreply.github.com>
2023-05-25 01:16:56 -0500

    Fix isolation in update bash scripts

--- update_linux.sh
+++ update_linux.sh
@@ -8,16 +8,16 @@ if [[ "$(pwd)" =~ " " ]]; then echo This script relies on Miniconda which can no
 CONDA_ROOT_PREFIX="$(pwd)/installer_files/conda"
 INSTALL_ENV_DIR="$(pwd)/installer_files/env"
 
-# activate installer env
-source "$CONDA_ROOT_PREFIX/etc/profile.d/conda.sh" # otherwise conda complains about 'shell not initialized' (needed when running in a script)
-conda activate "$INSTALL_ENV_DIR"
-
 # environment isolation
 export PYTHONNOUSERSITE=1
 unset PYTHONPATH
 unset PYTHONHOME
 export CUDA_PATH="$INSTALL_ENV_DIR"
 export CUDA_HOME="$CUDA_PATH"
+
+# activate installer env
+source "$CONDA_ROOT_PREFIX/etc/profile.d/conda.sh" # otherwise conda complains about 'shell not initialized' (needed when running in a script)
+conda activate "$INSTALL_ENV_DIR"
 
 # update installer env
 python webui.py --update
