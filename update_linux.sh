4d4ee5c6225b7d8c0ed4e16426140b3b9b103183
jllllll <3887729+jllllll@users.noreply.github.com>
2023-05-25 01:03:24 -0500

    Improve environment isolation

--- update_linux.sh
+++ update_linux.sh
@@ -12,6 +12,13 @@ INSTALL_ENV_DIR="$(pwd)/installer_files/env"
 source "$CONDA_ROOT_PREFIX/etc/profile.d/conda.sh" # otherwise conda complains about 'shell not initialized' (needed when running in a script)
 conda activate "$INSTALL_ENV_DIR"
 
+# environment isolation
+export PYTHONNOUSERSITE=1
+unset PYTHONPATH
+unset PYTHONHOME
+export CUDA_PATH="$INSTALL_ENV_DIR"
+export CUDA_HOME="$CUDA_PATH"
+
 # update installer env
 python webui.py --update
 
