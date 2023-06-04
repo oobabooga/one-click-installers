1737b36b190f3c234878900d81dacf53d2d4d294
jllllll <3887729+jllllll@users.noreply.github.com>
2023-06-04 01:52:03 -0500

    Restructure wsl.sh to support existing conda

--- wsl.sh
+++ wsl.sh
@@ -15,6 +15,8 @@ read -n1 -p "Continue the installer anyway? [y,n]" EXIT_PROMPT
 if ! [[ $EXIT_PROMPT == "Y" || $EXIT_PROMPT == "y" ]]; then exit; fi
 fi
 
+# deactivate any currently active conda env
+conda deactivate 2> /dev/null
 
 # config   unlike other scripts, can't use current directory due to file IO bug in WSL, needs to be in virtual drive
 INSTALL_DIR="$HOME/text-gen-install"
@@ -22,6 +24,22 @@ CONDA_ROOT_PREFIX="$INSTALL_DIR/installer_files/conda"
 INSTALL_ENV_DIR="$INSTALL_DIR/installer_files/env"
 MINICONDA_DOWNLOAD_URL="https://repo.anaconda.com/miniconda/Miniconda3-py310_23.1.0-1-Linux-x86_64.sh"
 conda_exists="F"
+
+# environment isolation
+export PYTHONNOUSERSITE=1
+unset PYTHONPATH
+unset PYTHONHOME
+export CUDA_PATH="$INSTALL_ENV_DIR"
+export CUDA_HOME="$CUDA_PATH"
+
+# /usr/lib/wsl/lib needs to be added to LD_LIBRARY_PATH to fix years-old bug in WSL where GPU drivers aren't linked properly
+export LD_LIBRARY_PATH="$CUDA_HOME/lib:/usr/lib/wsl/lib:$LD_LIBRARY_PATH"
+
+# open bash cli if called with 'wsl.sh cmd' with workarounds for existing conda
+if [ "$1" == "cmd" ]; then
+    exec bash --init-file <(echo ". ~/.bashrc; conda deactivate 2> /dev/null; cd $INSTALL_DIR || cd $HOME; source $CONDA_ROOT_PREFIX/etc/profile.d/conda.sh; conda activate $INSTALL_ENV_DIR")
+    exit
+fi
 
 if [[ "$INSTALL_DIR" =~ " " ]]; then echo This script relies on Miniconda which can not be silently installed under a path with spaces. && exit; fi
 
@@ -60,16 +78,6 @@ if [ ! -e "$INSTALL_ENV_DIR/bin/python" ]; then
     exit
 fi
 
-# environment isolation
-export PYTHONNOUSERSITE=1
-unset PYTHONPATH
-unset PYTHONHOME
-export CUDA_PATH="$INSTALL_ENV_DIR"
-export CUDA_HOME="$CUDA_PATH"
-
-# /usr/lib/wsl/lib needs to be added to LD_LIBRARY_PATH to fix years-old bug in WSL where GPU drivers aren't linked properly
-export LD_LIBRARY_PATH="$CUDA_HOME/lib:/usr/lib/wsl/lib:$LD_LIBRARY_PATH"
-
 # activate installer env
 source "$CONDA_ROOT_PREFIX/etc/profile.d/conda.sh" # otherwise conda complains about 'shell not initialized' (needed when running in a script)
 conda activate "$INSTALL_ENV_DIR"
@@ -77,6 +85,5 @@ conda activate "$INSTALL_ENV_DIR"
 # setup installer env   update env if called with 'wsl.sh update'
 case "$1" in
 ("update") python webui.py --update;;
-("cmd") exec bash;;
 (*) python webui.py;;
 esac
